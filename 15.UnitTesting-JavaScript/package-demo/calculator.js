4ff852b340cdc6546adc2ead47c7d1c9b247988a
Diyan Apostolov <diyan.apostolov@yahoo.com>
2025-08-08 16:43:09 +0300

    Add solutions for Unit Testing with JavaScript

--- 15.UnitTesting-JavaScript/package-demo/calculator.js
+++ 15.UnitTesting-JavaScript/package-demo/calculator.js
@@ -1,4 +1,8 @@
 function sum (a, b){
+    if (typeof a != 'number' || typeof b != 'number'){
+        throw new Error('Invalid parameter!')
+    }
+
     return a + b
  }
 
--- 15.UnitTesting-JavaScript/package-demo/test/calsulator.test.js
+++ 15.UnitTesting-JavaScript/package-demo/test/calsulator.test.js
@@ -20,5 +20,23 @@ describe('Demo tests', () => {
     }),
     it('Test divide function', () => {
         assert.equal(20, divide(100, 5))
+    }),
+    it('shoud throw an error if first parameter is not a number', () => {
+        expect(() => sum('a', 5)).to.throw
+        expect(() => sum([], 5)).to.throw
+        expect(() => sum({}, 5)).to.throw
+        expect(() => sum('5', 5)).to.throw('Invalid parameter!')
+
+        assert.throws(() => sum('a', 5), Error)
+        assert.throws(() => sum('a', 5), Error, 'Invalid parameter!') 
+    }),
+    it('shoud throw an error if second parameter is not a number', () => {
+        expect(() => sum(2, 'a')).to.throw
+        expect(() => sum(2, [])).to.throw
+        expect(() => sum(2, {})).to.throw
+        expect(() => sum(2, '5')).to.throw('Invalid parameter!')
+
+        assert.throws(() => sum(2, 'a'), Error)
+        assert.throws(() => sum(2, 'a'), Error, 'Invalid parameter!') 
     })
 })
